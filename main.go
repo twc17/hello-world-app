b09ad104192f2a944de7564b1cf0b9c3b485b7f2
Troy <twc17@pitt.edu>
2018-08-24 11:51:51 -0400

    init

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	version = "v0.0.1"
+)
+
+func main() {
+	log.Println("Starting hello-world application")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!\n -Troy\n")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, version)
+	})
+
+	s := http.Server{Addr: ":80"}
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown received, closing...")
+
+	s.Shutdown(context.Background())
+}
